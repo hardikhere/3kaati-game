a17f8b1b855710514ca7d11f9a195c4b950c3389
hardikhere <web.hardikhere@gmail.com>
2022-01-23 23:25:55 +0530

    whilist vercel link

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 const PORT = 8000;
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://3kaati.vercel.app/"],
     methods: ["GET", "POST"],
   },
 });
